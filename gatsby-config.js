3c8a5234e214a49a2abebd282bbe3b4c7263f799
Cole DeVries <colemandevries@gmail.com>
2022-10-30 15:15:34 -0400

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `gatsby-demo-site-october-22`,
+    title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+        <Layout pageTitle="My About Page">
+          <p>This is a page about me.</p>
+        </Layout>
+  )
+}
+
+export const Head = () => <Seo title="About Page" />
+// Step 3: Export your component
+export default AboutPage
